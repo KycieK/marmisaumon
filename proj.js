3ff8dba539e60f057d42f55eee8eedf0918ca632
KycieK <victor.leonard41@gmail.com>
2022-12-20 17:11:51 +0100

    2eme commit

--- proj.js
+++ proj.js
@@ -1,19 +1,22 @@
 var i = 0;
 function AddInput()
 {
-    i = i + 1;
-    var html = "<br/>";
+    var x = document.getElementById("ingredients");
     var input = document.createElement('input');
+
     input.setAttribute('type', 'text');
     input.placeholder = "nom ingrédient";
-    input.id = i;
     input.size = input.placeholder.length;
-    document.body.appendChild(input);
-    
+
+    i = i + 1;
+    input.id = i;
+
+    x.appendChild(input);
 }
 
 function RemoveInput()
 {
+    if(i==0){return alert("aucun ingrédient à supprimer")};
     var input = document.getElementById(i);
     input.remove();
     i = i-1;
